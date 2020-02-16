4e2abbf4e843153f0ff6ad5c59916f118f56439d
Aleksandr Pavlov <7911562+av-pavlov@users.noreply.github.com>
2020-02-16 17:33:40 +0900

    arb in progress

--- arbiter.py
+++ arbiter.py
@@ -2,9 +2,12 @@
 
 """ Проверка задачи на тестах из папки, запись результатов в файл """
 
-import os, shutil, sys, time, logging, glob
+import os, shutil, sys, time, logging, glob, re
 from argparse import ArgumentParser
 from collections import OrderedDict
+
+from multimeter._tasks import Task
+from ctypes import CDLL, c_char_p, c_uint, byref
 
 DEFAULT_MASK = 'Debug/*.exe'
 
@@ -38,6 +41,7 @@ def argparse():
         sys.exit(129)
 
 def check_writable(directory):
+    """ Проверка, что в каталог с конфиг-названием directory можно писать """
     global cfg
     try:
         fn = os.path.join(cfg[directory], '###qqq@@.json')
@@ -47,10 +51,11 @@ def check_writable(directory):
         logging.error(f'Не удалась попытка записи в {directory}-каталог "{cfg[directory]}"!!!')
         sys.exit(2)
 
-def check_dirs(cfg):
-    base_dir = os.getcwd()
-
+def check_dirs():
+    """ проверка наличия и доступности всех каталогов"""
+    global cfg
     for _ in ('workdir', 'testdir', 'resultsdir'):
+        base_dir = os.getcwd() if _=='workdir' else cfg['workdir']
         directory = cfg[_] = os.path.join(base_dir, cfg[_])
         if not os.path.isdir(directory):
             logging.error(f'Не удалось найти {_}-каталог "{directory}"!!!')
@@ -64,7 +69,9 @@ def check_dirs(cfg):
     check_writable('workdir')
     check_writable('resultsdir')
 
-def check_solution(cfg):
+def check_solution_exists():
+    """ Проверка наличия файла решений """
+    global cfg
     fn = None
     msg = 'Solution file not found!'
     if cfg['solution'] == DEFAULT_MASK:
@@ -79,147 +86,114 @@ def check_solution(cfg):
     if not (fn and os.path.isfile(fn)):
         logging.error(msg)
         sys.exit(2)
+    cfg['solution'] = fn
+
+def check_checker_exists():
+    """ Проверка наличия файла решений """
+    global cfg
+    fn = os.path.join(cfg['testdir'], 'check.exe')
+    if not os.path.isfile(fn):
+        logging.error(msg)
+        sys.exit(2)
+    cfg['checker'] = fn
+
+class PatchedTask(Task):
+    @property
+    def checker(self):
+        global cfg
+        return cfg['checker']
 
 
 def cleanup(task):
-        """ Очистка после проверки теста """
-        cleanup_list = [task.input_file, task.output_file, 'answer.txt', 'stdout']
-        while len(cleanup_list) > 0:
-            for filename in cleanup_list:
-                try:
-                    os.remove(filename)
-                    cleanup_list.remove(filename)
-                except FileNotFoundError:
-                    cleanup_list.remove(filename)
-                except PermissionError:
-                    pass
+    """ Очистка старых выходных данных перед запуском """
+    global cfg
+    cleanup_list = glob.glob(os.path.join(cfg['testdir'], '*.o'))
+    while cleanup_list:
+        filename = cleanup_list.pop():
+        try:
+            os.remove(filename)
+        except FileNotFoundError:
+            pass
+        except PermissionError:
+            logging.error('Не могу удалить файл выходных данных задачи: ' + filename)
+            sys.exit(2)
 
+def execute(task):
+    global cfg
+    answer = '??'
+    try:
+        executable = cfg['solution']
+        executable = c_char_p(executable.encode('utf-8'))
+        input_file = c_char_p(task.input_file.encode('utf-8'))
+        output_file = c_char_p(b'stdout')
+        memory_limit = c_uint(0)
+        time_limit = c_uint(int(1000 * task.timeout))
+        checktoolspath = os.path.split(__file__)[0]
+        dllpath = os.path.abspath(os.path.join(checktoolspath, 'invoker.dll'))
+        invoker = CDLL(dllpath)
+        invoker.console(executable, input_file, output_file, byref(memory_limit), byref(time_limit))
+        if memory_limit.value > task.memory_limit * 1024 * 1024:
+            answer = 'ML'
+        elif time_limit.value > task.time_limit * 1000:
+            answer = 'TL'
+        else:
+            answer = 'OK'
+    except subprocess.CalledProcessError:
+        answer = 'RE'  # Runtime error
+    except OSError:
+        answer = 'RE'  # Runtime error
+    except subprocess.TimeoutExpired:
+        if sys.getwindowsversion().major > 5:
+            subprocess.call("TaskKill /IM {}.exe /F".format(solution))
+        else:
+            subprocess.call("tskill {}".format(solution))  # Windows XP
+        answer = 'TL'  # Time Limit Exceeded
+    finally:
+        return answer
 
-def check_test(self, task, language, solution, test):
-    pass
-
-def check_solution2(self, task, language, solution):
-    """ Проверка решений """
+def check_solution():
+    """ Проверка решения """
+    global cfg
     answer = {
         "datetime": datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
-        "language": language.name,
-        "compilation": "CE",
-        "preliminary": OrderedDict(),
+        "language": "VisualCppLang",
+        "compilation": "FL",
         "results": OrderedDict()
     }
 
-    logging.info('')
-    logging.info("================================================================================")
-    logging.info("Popped from queue problem: %s, solution: %s, language: %s" % (task.code, solution, language.name))
+    code = re.sub('[^A-Za-z0-9_.]', '' , os.path.basename(cfg['workdir']))
+    task = PatchedTask(code, cfg['workdir'])
 
-    os.chdir(task.solutions_dir)
+    logging.info("Переходим в рабочий каталог")
+    os.chdir(cfg['workdir'])
 
-    # Компиляция
-    logging.info("Compiling... ")
-    answer['compilation'] = language.compile(solution)
-    if answer['compilation'][0] == 'OK':
-        logging.info("Compile OK")
-    else:
-        logging.info("Compile failed")
-        cfn = os.path.join(compile_messages_dir, '')
-        return answer
-
-    # Предварительная проверка
-    passed = True
-    for test in task.preliminary:
-        test_file = os.path.join(task.preliminary_dir, test)
+    # Проверка на тестах
+    results = []
+    testmask = os.path.join(cfg['testdir'], '??')
+    tests = sorted([os.path.basename(fn) for fn in glob.glob(testmask)])
+    suite_key = '.' # на будущее мб папки для позадач
+    answer['results'][suite_key] = OrderedDict()
+    for test in tests:
+        test_file = os.path.join(cfg.tests, suite_key, test)
         shutil.copy(test_file, task.input_file)
-        logging.info("Preliminary test %s... " % test)
-        execution_verdict = language.execute(task, solution)
+        logging.info(f'Запуск на тесте {test}:')
+        execution_verdict = execute(task)
         if execution_verdict != 'OK':
             verdict = execution_verdict
         else:
             shutil.copy(test_file + '.a', 'answer.txt')
             verdict = task.check()
-        answer['preliminary'][test] = verdict
+        answer['results'][suite_key][test] = verdict
         cleanup(task)
-        logging.info(verdict)
-        if verdict != 'OK':
-            passed = False
-    if not passed:
-        return answer
-
-    # Проверка на основных тестах
-    for suite_key, suite_value in task.test_suites.items():
-        logging.info("Test suite " + suite_key)
-
-        # Костыль про зависимость подзадач
-        if suite_value.depends:
-            shall_not_pass = False
-            for dependency in suite_value.depends:
-                for test, verdict in answer['results'][dependency].items():
-                    if verdict != 'OK':
-                        shall_not_pass = True
-                        break
-                if shall_not_pass:
-                    break
-            if shall_not_pass:
-                continue
-
-        answer['results'][suite_key] = OrderedDict()
-        for test in suite_value.tests:
-            test_file = os.path.join(task.test_suites_dir, suite_key, test)
-            shutil.copy(test_file, task.input_file)
-            logging.info("  checking test %s..." % test)
-            execution_verdict = language.execute(task, solution)
-            if execution_verdict != 'OK':
-                verdict = execution_verdict
-            else:
-                shutil.copy(test_file + '.a', 'answer.txt')
-                verdict = task.check()
-            answer['results'][suite_key][test] = verdict
-            self.cleanup(task)
-            logging.info(verdict)
+        logging.info(f'{test}: {verdict}')
 
     return answer
-
-def queue_is_empty(self):
-    """ Компиляция и проверка загруженных решений """
-    for filename in os.listdir(self.queue_dir):
-        print(filename)
-        (task_code, username, lang_code, attempt) = filename.split('-')
-        task = tasks[task_code]
-        language = languages[lang_code]
-        solution = '{}-{}'.format(username, attempt)
-
-        # Перемещение файла из очереди в папку решений
-        source_file = os.path.join(self.queue_dir, filename)
-        destination_file = os.path.join(task.solutions_dir, "{}.{}".format(solution, language.extension))
-        shutil.move(source_file, destination_file)
-
-        result = self.check_solution(task, language, solution)
-
-        total = 0
-        for key, values in result['results'].items():
-            settings = task.test_suites[key]
-
-            if settings.scoring == 'entire':
-                # Если подзадача оценивается по принципу "все или ничего"
-                correct = all([v == 'OK' for v in values.values()])
-                total += settings.total_score if correct else 0
-
-            elif settings.scoring == 'partial':
-                # Если подзадача оценивается пропорционально
-                correct = [settings.test_score for v in values.values() if v == 'OK']
-                total += sum(correct)
-
-        result['total'] = total
-
-        os.chdir(self.base_dir)
-        save_json(result, os.path.join(self.results_dir, '{}-{}-{}.json'.format(task_code, username, attempt)))
-
-    return True
 
 
 if __name__ == '__main__':
     logsetup()
     cfg = argparse()
     check_dirs(cfg)
-    check_solution(cfg)
+    check_solution_exists(cfg)
     logging.info("Arbiter started.")
-
+    check_solution()
