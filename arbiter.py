44c4cc073c94b3e3137064c239a13b3a96b03169
Aleksandr Pavlov <7911562+av-pavlov@users.noreply.github.com>
2020-02-17 15:08:28 +0900

    arbiter preflight

--- arbiter.py
+++ arbiter.py
@@ -2,14 +2,19 @@
 
 """ Проверка задачи на тестах из папки, запись результатов в файл """
 
-import os, shutil, sys, time, logging, glob, re, datetime
+import os, shutil, sys, time, logging, glob, re, datetime, subprocess
+from os.path import abspath, basename, split as pathsplit, join as pathjoin, isfile, isdir
 from argparse import ArgumentParser
 from collections import OrderedDict
-
-from multimeter._tasks import Task
 from ctypes import CDLL, c_char_p, c_uint, byref
 
+from multimeter._tasks import Task
+
 DEFAULT_SOLUTION_MASK = 'Debug/*.exe'
+INPUT_FILENAME  = 'putin1.txt'
+OUTPUT_FILENAME = 'putout.txt'
+ANSWER_FILENAME = 'putans.txt'
+TMPFILE_MASK =    'put???.txt'
 
 cfg = {}
 invoker = None
@@ -17,6 +22,34 @@ invoker = None
 
 class ArbiterError(Exception):
     pass
+
+
+class PatchedTask(Task):
+    input_file = INPUT_FILENAME
+    output_file = OUTPUT_FILENAME
+
+    @property
+    def checker(self):
+        global cfg
+        return cfg['checker']
+
+    def check(self):
+        """ Проверка ответа участника """
+        answer = 'FL'
+        try:
+            subprocess.check_call([
+                self.checker,
+                self.input_file,
+                self.output_file,
+                ANSWER_FILENAME,
+            ])
+            answer = 'OK'
+        except subprocess.CalledProcessError as error:
+            if error.returncode == 1:
+                answer = 'WA'  # Wrong answer
+            elif error.returncode == 2:
+                answer = 'PE'  # Presentation error
+        return answer
 
 
 def logsetup():
@@ -52,9 +85,9 @@ def check_writable(directory):
     """ Проверка, что в каталог с конфиг-названием directory можно писать """
     global cfg
     try:
-        fn = os.path.join(cfg[directory], '###qqq@@.json')
-        open(fn, 'w').close()
-        os.remove(fn)
+        solution = pathjoin(cfg[directory], '###qqq@@.json')
+        open(solution, 'w').close()
+        os.remove(solution)
     except OSError as error:
         logging.error(f'Не удалась попытка записи в {directory}-каталог "{cfg[directory]}"!!!')
         raise ArbiterError('FL') from None
@@ -64,11 +97,10 @@ def check_dirs():
     global cfg
     for _ in ('workdir', 'testdir', 'resultsdir'):
         base_dir = os.getcwd() if _=='workdir' else cfg['workdir']
-        directory = cfg[_] = os.path.join(base_dir, cfg[_])
-        if not os.path.isdir(directory):
+        directory = cfg[_] = pathjoin(base_dir, cfg[_])
+        if not isdir(directory):
             logging.error(f'Не удалось найти {_}-каталог "{directory}"!!!')
             raise ArbiterError('FL')
-
     try:
         os.chdir(cfg['workdir'])
     except OSError as error:
@@ -77,25 +109,28 @@ def check_dirs():
 
     check_writable('workdir')
     check_writable('resultsdir')
+    cfg['taskname'] = re.sub('[^A-Za-z0-9_.]', '' , basename(cfg['workdir']))
 
 def check_solution_exists():
     """ Проверка наличия файла решений """
     global cfg
-    fn = None
+    solpath = pathjoin(cfg['workdir'], cfg['solution'])
+    solution = None
     msg = 'Solution file not found!'
     if cfg['solution'] == DEFAULT_SOLUTION_MASK:
-        fn = glob.glob(os.path.join(cfg['workdir'], DEFAULT_SOLUTION_MASK))
-        if len(fn) == 1:
-            fn = fn[0]
+        fns = glob.glob(solpath)
+        if len(fns) == 1:
+            solution = fns[0]
+        elif not fns:
+            msg = f'Файл решения не найден ({solpath})'
         else:
             msg = 'Если файл решения не указан явно, он должен быть единственным exe-файлом в каталоге Debug!'
-            fn = None
     else:
-        fn = os.path.join(cfg['workdir'], cfg['solution'])
-    if not (fn and os.path.isfile(fn)):
+        solution = solpath
+    if not (solution and isfile(solution)):
         logging.error(msg)
         raise ArbiterError('FL')
-    cfg['solution'] = fn
+    cfg['solution'] = solution
 
 def check_checker_exists():
     """ Проверка наличия файла решений """
@@ -106,11 +141,12 @@ def check_checker_exists():
         raise ArbiterError('FL')
     cfg['checker'] = fn
 
-def check_invoker_exists():
+
+def check_invoker_loads():
     """ Проверка наличия invoker.dll """
     global cfg, invoker
-    dllpath = os.path.abspath(os.path.join(cfg['checktoolsdir'], 'invoker.dll'))
-    if not os.path.isfile(dllpath):
+    dllpath = abspath(pathjoin(cfg['checktoolsdir'], 'invoker.dll'))
+    if not isfile(dllpath):
         logging.error(f'Библиотека для запуска решений invoker.DLL ({dllpath}) не найдена!')
         raise ArbiterError('FL')
     try:
@@ -119,18 +155,10 @@ def check_invoker_exists():
         logging.error(f'Библиотека для запуска решений invoker.DLL ({dllpath}) не может быть загружена!')
         raise ArbiterError('FL') from None
 
-
-class PatchedTask(Task):
-    @property
-    def checker(self):
-        global cfg
-        return cfg['checker']
-
-
 def cleanup(task):
     """ Очистка старых выходных данных перед запуском """
     global cfg
-    cleanup_list = glob.glob(os.path.join(cfg['testdir'], '*.o'))
+    cleanup_list = glob.glob(pathjoin(cfg['workdir'], TMPFILE_MASK))
     while cleanup_list:
         filename = cleanup_list.pop()
         try:
@@ -138,21 +166,20 @@ def cleanup(task):
         except FileNotFoundError:
             pass
         except PermissionError:
-            logging.error('Не могу удалить файл выходных данных задачи: ' + filename)
-            sys.exit(2)
+            logging.error('Не могу удалить временные файл(ы) теста: ' + filename)
+            raise ArbiterError('FL') from None
 
 def execute(task):
     global cfg
-    answer = '??'
-    try:
-        executable = cfg['solution']
-        executable = c_char_p(executable.encode('utf-8'))
-        input_file = c_char_p(task.input_file.encode('utf-8'))
-        output_file = c_char_p(b'stdout')
-        memory_limit = c_uint(0)
-        time_limit = c_uint(int(1000 * task.timeout))
+    answer = 'FL'
 
-        invoker.console(executable, input_file, output_file, byref(memory_limit), byref(time_limit))
+    files = [c_char_p(fn.encode('utf-8')) 
+             for fn in (cfg['solution'], task.input_file, task.output_file)]
+    memory_limit = c_uint(0)
+    time_limit = c_uint(int(1000 * task.timeout))
+        
+    try:
+        invoker.console(*files, byref(memory_limit), byref(time_limit))
 
         if memory_limit.value > task.memory_limit * 1024 * 1024:
             answer = 'ML'
@@ -170,56 +197,72 @@ def execute(task):
         else:
             subprocess.call("tskill {}".format(solution))  # Windows XP
         answer = 'TL'  # Time Limit Exceeded
-    finally:
-        return answer
+    return answer
 
 def check_solution():
     """ Проверка решения """
     global cfg
     answer = {
-        "datetime": datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
-        "language": "VisualCppLang",
-        "compilation": "FL",
-        "results": OrderedDict()
+        'datetime': datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
+        'language': 'VisualCppLang',
+        'compilation': 'FL',
+        'results': OrderedDict()
     }
+    
+    task = PatchedTask(cfg['taskname'], cfg['workdir'])
+    task.input_file
 
-    code = re.sub('[^A-Za-z0-9_.]', '' , os.path.basename(cfg['workdir']))
-    task = PatchedTask(code, cfg['workdir'])
-
-    logging.info("Переходим в рабочий каталог " + cfg['workdir'])
+    logging.info('Переходим в рабочий каталог ' + cfg['workdir'])
     os.chdir(cfg['workdir'])
 
     # Проверка на тестах
     results = []
-    testmask = os.path.join(cfg['testdir'], '??')
-    tests = sorted([os.path.basename(fn) for fn in glob.glob(testmask)])
-    logging.info("Найдены тесты: "+' '.join(tests))
+    testmask = pathjoin(cfg['testdir'], '??')
+    tests = sorted([basename(fn) for fn in glob.glob(testmask)])
+    logging.info('Найдены тесты: ' + ' '.join(tests))
+    
     suite_key = '.' # на будущее мб папки для позадач
     answer['results'][suite_key] = OrderedDict()
+    
+    first_run_timelimit = 3
+    task.time_limit, task.timeout = first_run_timelimit, 2*first_run_timelimit
     for test in tests:
-        test_file = os.path.join(cfg['testdir'], suite_key, test)
+        test_file = pathjoin(cfg['testdir'], suite_key, test)
         shutil.copy(test_file, task.input_file)
-        logging.info(f'Запуск на тесте {test}:')
         execution_verdict = execute(task)
         if execution_verdict != 'OK':
             verdict = execution_verdict
         else:
-            shutil.copy(test_file + '.a', 'answer.txt')
+            shutil.copy(test_file + '.a', ANSWER_FILENAME)
             verdict = task.check()
         answer['results'][suite_key][test] = verdict
         cleanup(task)
-        logging.info(f'{test}: {verdict}')
-
-    return answer
-
+        logging.info(f'Тест {test}: {verdict}')
+        if verdict != 'OK':
+            logging.info('Останавливаю тестирование.')
+            raise ArbiterError(verdict)
+        task.time_limit, task.timeout = 1.5, 3
+    logging.info('=== Тестирование завершено ===\nРезультаты:\n' + pprint.pformat(answer))
+    return verdict
 
 if __name__ == '__main__':
-    logsetup()
-    cfg = argparse()
-    cfg['checktoolsdir'] = os.path.split(os.path.abspath(__loader__.path))[0]    
-    check_dirs()
-    check_checker_exists()
-    check_solution_exists()
-    check_invoker_exists()
-    logging.info("Arbiter started.")
-    check_solution()
+    try:
+        logsetup()
+        cfg = argparse()
+        cfg['checktoolsdir'] = os.path.split(abspath(__loader__.path))[0]
+        cfg['taskname'] = re.sub('[^A-Za-z0-9_.]', '' , basename(abspath(cfg['workdir'])))
+
+        check_dirs()
+        check_checker_exists()
+        check_solution_exists()
+        check_invoker_loads()
+        
+        logging.info(f'=== Тестирование задачи {cfg["taskname"]} начато ===')
+        result = check_solution() 
+    except ArbiterError as e:
+        result = e.args[0]
+    logging.info(f'=== Тестирование задачи {cfg["taskname"]} завершено: {result} ===')
+    try:
+        open(pathjoin(cfg['resultsdir'], cfg['taskname']+'.res'), 'w').write(result)
+    except:
+        pass
