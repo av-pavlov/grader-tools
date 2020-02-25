125afa7f38e1ee682ef03ec4a277a207f4847591
Alexander Pavlov <7911562+av-pavlov@users.noreply.github.com>
2020-02-25 15:48:15 +0900

    reporter

--- test_indiv.py
+++ test_indiv.py
@@ -1,2 +1,13 @@
-f = open("README.md", "a+")
-f.write("Ran test_indiv.py one time.\n\n")
+MARKER = r'[//]: # (INDIV)'
+
+def build_badges():
+	return "Возможно, со временем появится автоматическое тестирование индивидуальных заданий"
+
+report = ""
+for line in open("README.md", encoding="utf-8"):
+	if MARKER in line:
+		report += MARKER + ' ' + build_badges()
+	else
+		report += line
+
+open("README.md", "w", encoding="utf-8").write(report)
