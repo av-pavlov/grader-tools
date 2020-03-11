3c4c3552afd49fece404f6b4d829168960f5ee82
Aleksandr Pavlov <7911562+av-pavlov@users.noreply.github.com>
2020-03-12 02:29:14 +0900

    add new build2.cmd to move the logic out of action ymls

---
+++ build2.cmd
@@ -0,0 +1,11 @@
+@echo off
+set INCLUDE=C:\Program Files (x86)\Microsoft SDKs\Windows\V7.1A\Include;%GITHUB_WORKSPACE%\tools
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x86
+echo ПЕРЕХОЖУ В ПАПКУ %GITHUB_WORKSPACE%\lab\%1
+echo off
+cd %GITHUB_WORKSPACE%\lab\%1
+rmdir /S /Q Debug
+echo CE>%1.res
+set TheToolset=v142
+set MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
+%MSBUILD% /t:rebuild /p:ForceImportBeforeCppTargets=%GITHUB_WORKSPACE%\tools\nosecure.props;Configuration=Debug;Platform=Win32;PlatformToolset=%TheToolset%;WindowsTargetPlatformVersion=10 /m /nologo /clp:Summary
