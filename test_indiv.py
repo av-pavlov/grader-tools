b649696c1686fda48087c8ff79f007d824f7e30e
Alexander Pavlov <7911562+av-pavlov@users.noreply.github.com>
2020-03-13 13:53:51 +0900

    indiv

--- test_indiv.py
+++ test_indiv.py
@@ -4,10 +4,15 @@ def build_badges():
 	return "Возможно, со временем появится автоматическое тестирование индивидуальных заданий"
 
 report = ""
+changed = False
 for line in open("README.md", encoding="utf-8"):
 	if MARKER in line:
-		report += f'{MARKER} {build_badges()}\n'
+		replacement_line = f'{MARKER} {build_badges()}\n'
+		if line != replacement_line:
+			changed = True
+		report += replacement_line
 	else:
 		report += line
 
-open("README.md", "w", encoding="utf-8").write(report)
+if changed:
+	open("README.md", "w", encoding="utf-8").write(report)
